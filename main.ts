355370bc8f0351f56711c24886da1b43ed52d65e
seoyeonpyo <117896485+seoyeonpyo@users.noreply.github.com>
2022-11-10 16:45:46 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.rotation(Rotation.Pitch))
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.temperature()))
+})
+input.onGesture(Gesture.LogoUp, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
+basic.forever(function () {
+	
+})
